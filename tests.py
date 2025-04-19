7007393dd78e3702c284b731604c6257ac68d5fa
Nahuel <nahuel_zabala@live.com>
2025-04-19 15:31:29 +0200

    borrador

--- tests.py
+++ tests.py
@@ -42,43 +42,66 @@
 # hexadecimal_decimal("1F")
 
 
-def binario_octa():
-    octal=""
-    num=str(1100011)
+# def binario_octa():
+#     octal=""
+#     num=str(1100011)
 
-    #Añadir 0 si el largo de la funcion no es multiplo de 3 para poder dividir el binario en secciones de 3
-    while len(num)%3!=0:
-        num="0"+num
+#     #Añadir 0 si el largo de la funcion no es multiplo de 3 para poder dividir el binario en secciones de 3
+#     while len(num)%3!=0:
+#         num="0"+num
 
-    grupos = [num[i:i+3] for i in range(0, len(num), 3)]
-    for i in grupos:
-        if len(i)==3:
-            octal=octal+str(binario_decimal(int(i)))
-        else:
-            while len(i)!=3:
-                i="0"+i
+#     grupos = [num[i:i+3] for i in range(0, len(num), 3)]
+#     for i in grupos:
+#         if len(i)==3:
+#             octal=octal+str(binario_decimal(int(i)))
+#         else:
+#             while len(i)!=3:
+#                 i="0"+i
                 
-            octal=octal+str(binario_decimal(int(i)))
-    print (octal)
+#             octal=octal+str(binario_decimal(int(i)))
+#     print (octal)
 
 
 
-def binario_decimal(num):
-    contador = 1
-    numero = num
-    decimal = 0
-    while num >=1:
-        cifra = num / 10
-        cifra = round(cifra % 1 * 10)
-        if cifra == 1:
-            decimal += contador
-        num = int(num / 10)
-        contador *= 2
-    print(f"BASE BINARIO: {numero} - BASE DECIMAL: {decimal}")
-    return decimal
+# def binario_decimal(num):
+#     contador = 1
+#     numero = num
+#     decimal = 0
+#     while num >=1:
+#         cifra = num / 10
+#         cifra = round(cifra % 1 * 10)
+#         if cifra == 1:
+#             decimal += contador
+#         num = int(num / 10)
+#         contador *= 2
+#     print(f"BASE BINARIO: {numero} - BASE DECIMAL: {decimal}")
+#     return decimal
                 
     
 
         
     
-binario_octa()
+# binario_octa()
+
+
+
+# def validar_numero(num):
+#     if original==binario:
+#         binario="01"
+#         for i in num:
+#             if i not in binario:
+#                 return False      
+#         return True
+
+
+
+# num=input("ingrese numero valido ")   
+
+# while not validar_numero(num):
+#     num=input("ingrese numero valido ")
+
+
+l="954"
+for i in range(len(l)):
+    print(i)
+               
